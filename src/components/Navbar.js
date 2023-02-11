be28194007c1991f11c1d5db455968a2fbd7fdd6
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-02-11 12:19:35 +0530

    test

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container">
+                    <a class="navbar-brand" href="/">
+                        <img src="C:/Users/ASUS/Downloads/ae.jpg" alt="Bootstrap" width="30" height="24" />
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
