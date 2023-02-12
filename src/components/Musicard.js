139e3cf9558e1d14ef58136a3bfc4897240e3fb7
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-02-12 20:05:02 +0530

    added hover effect

--- src/components/Musicard.js
+++ src/components/Musicard.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Musicard= ()=>{
     return (
         <>
-            <div className="container my-4">
-            <div className="card" style={{width: "18rem"}}>
+            <div className="container my-4 text-center "  >
+            <div className="card grow" style={{width: "18rem",border:"2px solid #FF1E1E"}}>
                 <img src="https://wallpaperboat.com/wp-content/uploads/2021/09/20/78678/waifu-18.jpg" className="card-img-top" alt="..." />
-                <div className="card-body">
+                <div className="card-body" style={{backgroundColor:"black"}}>
                     <h5 className="card-title">Card title</h5>
                     <button className="btn btn-primary">Go somewhere</button>
                 </div>
